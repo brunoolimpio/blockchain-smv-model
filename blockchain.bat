89f4304dada3b25cf54d02efac01a37b863975c4
Bruno Olímpio <bruno.olimpiocosta@gmail.com>
2018-09-04 00:27:49 -0300

    1- Passando a utilizar o arquivo 4; 2- Refatorando o P2PNetwork para tornar-se o createNode; 3- Iniciando validações para criar o módulo updateNode; 4- Incluindo parâmetro blockId na transação, para validar se já foi incluída em um bloco; 5- Voltando a usar "process" para instanciar algumas transações, para validar a criação de blocos.

---
+++ blockchain.bat
@@ -0,0 +1,3 @@
+cls
+@ECHO --- ABRINDO NUXMV ---
+"C:\Program Files (x86)\nuXmv-1.1.1-win64\bin\nuXmv.exe"  "C:\Users\Bruno\Dropbox\Mestrado\Lógica e especificação\model-checking-blockchain\blockchain3.smv";

7cc852ded443f7e89c59232455cc8b8fe595bfb9
Bruno Olímpio <bruno.olimpiocosta@gmail.com>
2018-10-18 10:54:37 -0300

    Excluindo arquivos antigos e criando um novo modelo formal de blockchain mais simples e funcional.

--- blockchain.bat
+++ blockchain.bat
@@ -1,3 +1,3 @@
 cls
 @ECHO --- ABRINDO NUXMV ---
-"C:\Program Files (x86)\nuXmv-1.1.1-win64\bin\nuXmv.exe"  "C:\Users\Bruno\Dropbox\Mestrado\Lógica e especificação\model-checking-blockchain\blockchain3.smv";
+"C:\Program Files (x86)\nuXmv-1.1.1-win64\bin\nuXmv.exe" -int "C:\Users\Bruno\Dropbox\Mestrado\Lógica e especificação\model-checking-blockchain\blockchain4.smv";

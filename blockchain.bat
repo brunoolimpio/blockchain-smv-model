c87a282c03c5eab652a251bca0b4c506edc8964d
Bruno Olímpio <bruno.olimpiocosta@gmail.com>
2019-04-27 00:09:42 -0300

    Deleting unuseful file

--- blockchain.bat
+++
@@ -1,3 +0,0 @@
-cls
-@ECHO --- ABRINDO NUXMV ---
-.\nuXmv.exe -source  "C:\Users\Bruno\Dropbox\Mestrado\Lógica e especificação\model-checking-blockchain\command.txt" "C:\Users\Bruno\Dropbox\Mestrado\Lógica e especificação\model-checking-blockchain\blockchain5.smv"
